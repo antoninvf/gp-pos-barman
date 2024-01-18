b608694dd8dd2b3c337f006b307ff832ee78262d
Antonín Vondrovic <flowythe@gmail.com>
2024-01-18 19:48:02 +0100

    added QueryClientProvider

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,19 +1,22 @@
-import "@mantine/core/styles.css";
+import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider } from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { ModalsProvider } from '@mantine/modals';
-import { theme } from "../../theme";
+import { theme } from '../../theme';
+import { QueryClientProvider } from '../modules/api';
 
 export default function App({ Component, pageProps }: any) {
-  return (
-    <MantineProvider theme={theme}>
-      <ModalsProvider>
-        <Notifications />
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </ModalsProvider>
-    </MantineProvider>
-  );
+	return (
+		<QueryClientProvider>
+			<MantineProvider theme={theme}>
+				<ModalsProvider>
+					<Notifications />
+					<main>
+						<Component {...pageProps} />
+					</main>
+				</ModalsProvider>
+			</MantineProvider>
+		</QueryClientProvider>
+	);
 }
