87eb6454e36a61f4a22ba2a1651f607dc85158c2
Antonín Vondrovic <flowythe@gmail.com>
2024-01-18 19:48:26 +0100

    auto incrementing primary keys + moved where auto migration happens

--- be-barman/be-barman/Data/ApplicationDbContext.cs
+++ be-barman/be-barman/Data/ApplicationDbContext.cs
@@ -7,7 +7,7 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
-        Database.Migrate();
+        //Database.Migrate();
     }
 
     public DbSet<KitchenQueueEntity> KitchenQueueEntities { get; set; }
@@ -18,8 +18,17 @@ public class ApplicationDbContext : DbContext
     {
         base.OnModelCreating(modelBuilder);
 
+        // set up primary keys
         modelBuilder.Entity<KitchenQueueEntity>().HasKey(x => x.UUID);
         modelBuilder.Entity<ProductEntity>().HasKey(x => x.UUID);
         modelBuilder.Entity<TableEntity>().HasKey(x => x.ID);
+        
+        // set up auto-incrementing primary keys
+        modelBuilder.Entity<KitchenQueueEntity>().Property(x => x.UUID).ValueGeneratedOnAdd();
+        modelBuilder.Entity<ProductEntity>().Property(x => x.UUID).ValueGeneratedOnAdd();
+        modelBuilder.Entity<TableEntity>().Property(x => x.ID).ValueGeneratedOnAdd();
+        
+        // set time added to current timestamp
+        modelBuilder.Entity<KitchenQueueEntity>().Property(x => x.Timestamp).HasDefaultValueSql("CURRENT_TIMESTAMP");
     }
 }
