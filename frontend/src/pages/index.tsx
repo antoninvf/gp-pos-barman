05ee4963eac84a6a9bc13b63633178d2b6e7c2c2
Antonín Vondrovic <flowythe@gmail.com>
2024-01-18 20:11:43 +0100

    temp login btn

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,19 @@
-import { Button, Flex, Group } from "@mantine/core";
-import Link from "next/link";
-import Navigation from "../modules/components/SidebarNav/Navigation";
+import { Button, Flex, Group } from '@mantine/core';
+import Link from 'next/link';
+import Navigation from '~components/SidebarNav/Navigation';
 
 export default function Home() {
-  return (
-    <Flex>
-      <Navigation />
-      <Flex direction={"column"} p={"1rem"}>
-      </Flex>
-    </Flex>
-  );
+	return (
+		<Flex>
+			<Navigation />
+			<Flex direction={'column'} p={'1rem'}>
+				temp dashboard
+				<Group>
+					<Link href={'/auth/login'}>
+						<Button>Log in</Button>
+					</Link>
+				</Group>
+			</Flex>
+		</Flex>
+	);
 }
