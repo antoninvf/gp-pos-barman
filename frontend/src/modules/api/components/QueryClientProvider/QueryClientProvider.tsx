93f632443a1360c6dcf38b07b015927ba7340bc3
Antonín Vondrovic <flowythe@gmail.com>
2024-01-18 20:10:22 +0100

    Update QueryClientProvider.tsx

--- frontend/src/modules/api/components/QueryClientProvider/QueryClientProvider.tsx
+++ frontend/src/modules/api/components/QueryClientProvider/QueryClientProvider.tsx
@@ -2,21 +2,28 @@
 
 import type { PropsWithChildren } from 'react';
 import { useState } from 'react';
-import { QueryClient, QueryClientProvider as RaQueryClientProvider } from '@tanstack/react-query';
+import {
+	QueryClient,
+	QueryClientProvider as RaQueryClientProvider,
+} from '@tanstack/react-query';
 
 interface QueryClientProps extends PropsWithChildren {}
 
 export const QueryClientProvider = ({ children }: QueryClientProps) => {
-  const [queryClient] = useState(
-    () =>
-      new QueryClient({
-        defaultOptions: {
-          queries: {
-            refetchInterval: false,
-          },
-        },
-      })
-  );
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchInterval: 1000,
+					},
+				},
+			}),
+	);
 
-  return <RaQueryClientProvider client={queryClient}>{children}</RaQueryClientProvider>;
+	return (
+		<RaQueryClientProvider client={queryClient}>
+			{children}
+		</RaQueryClientProvider>
+	);
 };
