d07306b7e9cd2a301041bfabcf3787f49e2aedcd
Antonin <flowythe@gmail.com>
2024-01-19 06:47:43 +0100

    item now shows the name of the product by ID

--- frontend/src/modules/kitchenQueue/components/KitchenQueueItem/KitchenQueueItem.tsx
+++ frontend/src/modules/kitchenQueue/components/KitchenQueueItem/KitchenQueueItem.tsx
@@ -1,5 +1,8 @@
-import { Badge, Card, Flex, Group, Text, Tooltip } from '@mantine/core';
+import { ActionIcon, Badge, Card, Flex, Text } from '@mantine/core';
+import { IconCheckbox, IconExclamationCircle } from '@tabler/icons-react';
 import moment from 'moment';
+import { apiHooks } from '~api';
+import { useRemoveFromKitchenQueue } from '~kitchenQueue/hooks';
 
 interface IKitchenQueueProps {
 	uuid?: string | null;
@@ -9,23 +12,49 @@ interface IKitchenQueueProps {
 }
 
 export const KitchenQueueItem = ({ ...props }: IKitchenQueueProps) => {
+	if (!props.uuid || !props.productID) return null;
+
+	const { data: product } = apiHooks.useGetProductUuid({
+		params: { uuid: props.productID },
+	});
+
+	const { mutate, isLoading } = useRemoveFromKitchenQueue({
+		uuid: props.uuid,
+		name: product?.name ? product?.name : props.productID,
+	});
+
 	return (
 		<Card shadow="sm" padding="lg" radius="sm" withBorder>
-			<Flex justify="space-between" align={'center'} gap={'6rem'}>
-				<Text fw={500} size="xl">
-					{props.productID}
+			<Flex justify="space-between" align={'start'} gap={'6rem'}>
+				<Text fw={600} size="xl">
+					{product?.name ? product?.name : props.productID}
 				</Text>
-				{<Badge color="pink">{moment(props.timestamp).fromNow()}</Badge>}
+				<Flex direction={'column'} gap={'0.1rem'}>
+					{!product?.name && <Badge color="#FF0000">Product error</Badge>}
+					{<Badge color="pink">{moment(props.timestamp).fromNow()}</Badge>}
+				</Flex>
 			</Flex>
-
-			{props.note && (
-				<>
-					<Text size="lg" c="dimmed">
-						Note:
-					</Text>
-					<Text size="lg">{props.note}</Text>
-				</>
-			)}
+			<Flex justify="space-between" align={'end'} gap={'6rem'}>
+				<Flex direction={'column'}>
+					{props.note && (
+						<>
+							<Text size="lg" mt={'sm'} c="dimmed">
+								Note:
+							</Text>
+							<Text size="lg">{props.note}</Text>
+						</>
+					)}
+				</Flex>
+				<ActionIcon
+					mt={'auto'}
+					mb={0}
+					size={'xl'}
+					color="green"
+					onClick={() => mutate(undefined)}
+				>
+					<IconCheckbox />
+				</ActionIcon>
+			</Flex>
 		</Card>
 	);
 };
