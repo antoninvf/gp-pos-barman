8f02269746e416c08e2d59232e06f33130160101
Antonin <flowythe@gmail.com>
2024-01-19 06:44:24 +0100

    added hidden param for future use + vw to vh

--- frontend/src/modules/components/BarmanLogo/BarmanLogo.tsx
+++ frontend/src/modules/components/BarmanLogo/BarmanLogo.tsx
@@ -5,13 +5,26 @@ import { IconBottleFilled } from '@tabler/icons-react';
 type BarmanLogoProps = {
 	size: number;
 	padding?: number;
+	hidden?: boolean;
 };
 
-export default function BarmanLogo({ size, padding = 0 }: BarmanLogoProps) {
+export default function BarmanLogo({
+	size,
+	padding = 0,
+	hidden,
+}: BarmanLogoProps) {
+	if (hidden) {
+		return (
+			<Flex align={'center'} p={padding * 0.5 + 'rem'}>
+				<IconBottleFilled size={size * 0.5 + 'vh'} />
+			</Flex>
+		);
+	}
+
 	return (
-		<Flex align={'center'} p={padding + 'rem'}>
-			<IconBottleFilled size={size + 'vw'} />
-			<Title size={size + 'vw'}>Barman</Title>
+		<Flex align={'center'} p={padding + 'rem'} pl={0}>
+			<IconBottleFilled size={size + 'vh'} />
+			<Title size={size + 'vh'}>Barman</Title>
 		</Flex>
 	);
 }
