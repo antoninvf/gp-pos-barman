edd8c218e1116ec24e931927330cdbf2695825e1
Antonin <flowythe@gmail.com>
2024-01-19 06:44:45 +0100

    work in progress configuration entity

---
+++ be-barman/be-barman/Entities/ConfigurationEntity.cs
@@ -0,0 +1,8 @@
+namespace be_barman.Entities;
+
+// This entity is used for storing settings and configuration
+public class ConfigurationEntity
+{
+    public string settingName { get; set; }
+    public string value { get; set; }
+}
