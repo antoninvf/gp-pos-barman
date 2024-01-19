bce7484d93f1cd0eb3d5463cfe1ea73f808bfb6a
Antonin <flowythe@gmail.com>
2024-01-19 06:47:03 +0100

    added "No orders right now" text, when there are no orders

--- frontend/src/modules/kitchenqueue/components/KitchenQueueList/KitchenQueueList.tsx
+++ frontend/src/modules/kitchenqueue/components/KitchenQueueList/KitchenQueueList.tsx
@@ -1,4 +1,5 @@
-import { Flex } from '@mantine/core';
+import { Flex, Title } from '@mantine/core';
+import { IconClockPause } from '@tabler/icons-react';
 import { apiHooks } from '~/api';
 import { KitchenQueueItem } from '~kitchenQueue';
 
@@ -7,12 +8,27 @@ interface IKitchenQueueProps {}
 export const KitchenQueueList = (props: IKitchenQueueProps) => {
 	const { data } = apiHooks.useGetKitchenQueue();
 
+	if (!data?.length)
+		return (
+			<Flex
+				w={'100%'}
+				h={'100%'}
+				align={'center'}
+				justify={'center'}
+				gap={'lg'}
+			>
+				<IconClockPause size={'4rem'} />
+				<Title>No orders right now</Title>
+			</Flex>
+		);
+
 	return (
 		<Flex wrap="wrap" gap={'sm'}>
 			{data?.map(({ uuid, productID, note, timestamp }) => {
 				if (!timestamp) return null;
 				return (
 					<KitchenQueueItem
+						key={uuid}
 						uuid={uuid}
 						productID={productID}
 						note={note}
