47491175ec703fd73f5932f79fef1a8564954451
Antonin <flowythe@gmail.com>
2024-01-19 06:46:02 +0100

    changed table entity to have roomID

--- be-barman/be-barman/Entities/TableEntity.cs
+++ be-barman/be-barman/Entities/TableEntity.cs
@@ -2,9 +2,9 @@ namespace be_barman.Entities;
 
 public class TableEntity
 {
-    public int ID { get; set; } // Tables are autoincremented IDs for easier use
+    public int? ID { get; set; } // Tables are autoincremented IDs for easier use
     public required string Name { get; set; }
-    public required string Room { get; set; }
+    public required string RoomID { get; set; }
     public int PositionX { get; set; }
     public int PositionY { get; set; }
 }
