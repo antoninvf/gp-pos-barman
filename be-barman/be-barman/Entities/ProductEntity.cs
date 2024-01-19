61afd1f74f9cb3a56fb9ffab357d8d0e206af380
Antonin <flowythe@gmail.com>
2024-01-19 06:46:15 +0100

    changed product entity to what its supposed to be

--- be-barman/be-barman/Entities/ProductEntity.cs
+++ be-barman/be-barman/Entities/ProductEntity.cs
@@ -2,10 +2,10 @@ namespace be_barman.Entities;
 
 public class ProductEntity
 {
-    public string UUID = Guid.NewGuid().ToString();
+    public required string ID { get; set; }
     public required string Name { get; set; }
     public string? Category { get; set; }
     public string? Description { get; set; }
     public string? ImageURL { get; set; }
-    public int Price { get; set; }
+    public required int Price { get; set; }
 }
