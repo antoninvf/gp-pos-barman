0a4b3321cafbbe7638fc7c36ab84da7d89a0268a
Antonin <flowythe@gmail.com>
2024-01-19 06:46:31 +0100

    revamped navigation with new icons and new links

--- frontend/src/modules/components/SidebarNav/Navigation.tsx
+++ frontend/src/modules/components/SidebarNav/Navigation.tsx
@@ -1,67 +1,84 @@
-import { Flex, NavLink, Paper } from '@mantine/core';
-import { IconGauge, IconToolsKitchen } from '@tabler/icons-react';
+import { Divider, Flex, NavLink, Paper } from '@mantine/core';
+import {
+	IconBorderAll,
+	IconBoxSeam,
+	IconCat,
+	IconChefHat,
+	IconGauge,
+	IconGlassFull,
+	IconToolsKitchen,
+} from '@tabler/icons-react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import BarmanLogo from '~components/BarmanLogo/BarmanLogo';
 
-export default function Navigation() {
+interface NavigationProps {}
+
+export default function Navigation({}: NavigationProps) {
 	const router = useRouter();
+	const iconSize = '2rem';
 
 	const pages = [
 		{
-			icon: <IconGauge size="1rem" stroke={1.5} />,
+			icon: <IconGlassFull size={iconSize} stroke={1.5} />,
 			label: 'Dashboard',
 			href: '/',
 		},
 		{
-			icon: <IconToolsKitchen size="1rem" stroke={1.5} />,
+			icon: <IconBoxSeam size={iconSize} stroke={1.5} />,
+			label: 'Products',
+			href: '/products',
+		},
+		{
+			icon: <IconBorderAll size={iconSize} stroke={1.5} />,
+			label: 'Tables',
+			href: '/tables',
+		},
+	];
+
+	const bottomPages = [
+		{
+			icon: <IconCat size={iconSize} stroke={1.5} />,
+			label: 'debug',
+			href: '/admin/debug',
+		},
+		{
+			icon: <IconChefHat size={iconSize} stroke={1.5} />,
 			label: 'Kitchen',
 			href: '/kitchen',
 		},
 	];
 
-	const adminPages = [
-		{
-			icon: <IconGauge size="1rem" stroke={1.5} />,
-			label: 'debug',
-			href: '/admin/debug',
-		},
-	];
-
 	return (
-		<Paper
-			component={Flex}
-			shadow="lg"
-			direction={'column'}
-			w={'15vw'}
-			h={'100vh'}
-			align={'center'}
-			bg={'white'}
-		>
-			<BarmanLogo size={3} padding={1} />
-			{pages.map((link, index) => (
-				<NavLink
-					key={index}
-					p={'1rem'}
-					label={link.label}
-					leftSection={link.icon}
-					component={Link}
-					href={link.href}
-					active={router.pathname === link.href}
-				></NavLink>
-			))}
-
-			{adminPages.map((link, index) => (
-				<NavLink
-					key={index}
-					p={'1rem'}
-					label={link.label}
-					leftSection={link.icon}
-					component={Link}
-					href={link.href}
-					active={router.pathname === link.href}
-				></NavLink>
-			))}
+		<Paper shadow="lg" w={'30vh'} h={'100vh'} bg={'white'}>
+			<Flex direction={'column'} align={'center'} w={'30vh'} h={'100vh'}>
+				<BarmanLogo size={5} padding={1} />
+				<Divider w={'95%'} opacity={0.5} />
+				{pages.map((link, index) => (
+					<NavLink
+						key={index}
+						p={'1rem'}
+						label={link.label}
+						leftSection={link.icon}
+						component={Link}
+						href={link.href}
+						active={router.pathname === link.href}
+					></NavLink>
+				))}
+				<Flex mt={'auto'} direction={'column'} w={'100%'}>
+					{bottomPages.map((link, index) => (
+						<NavLink
+							key={index}
+							p={'1rem'}
+							label={link.label}
+							leftSection={link.icon}
+							component={Link}
+							href={link.href}
+							active={router.pathname === link.href}
+						></NavLink>
+					))}
+				</Flex>
+			</Flex>
 		</Paper>
 	);
 }
