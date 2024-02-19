b40761181a3ed62fb312cfd2b14f92bfe93c2ee0
Antonin <flowythe@gmail.com>
2024-02-19 19:17:06 +0100

    migrations

---
+++ be-barman/be-barman/Migrations/20240219155429_init.cs
@@ -0,0 +1,191 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace be_barman.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ConfigurationEntities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    SettingName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Value = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConfigurationEntities", x => x.ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "ProductEntities",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Category = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Description = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ImageURL = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    SendToKitchenQueue = table.Column<bool>(type: "tinyint(1)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductEntities", x => x.ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "TableEntities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Room = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableEntities", x => x.ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "CustomerEntities",
+                columns: table => new
+                {
+                    UUID = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    TableID = table.Column<int>(type: "int", nullable: false),
+                    CreationTimestamp = table.Column<long>(type: "bigint", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerEntities", x => x.UUID);
+                    table.ForeignKey(
+                        name: "FK_CustomerEntities_TableEntities_TableID",
+                        column: x => x.TableID,
+                        principalTable: "TableEntities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "OrderEntities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    CustomerUUID = table.Column<string>(type: "varchar(255)", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    ProductID = table.Column<string>(type: "varchar(255)", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    Timestamp = table.Column<long>(type: "bigint", nullable: false),
+                    Notes = table.Column<string>(type: "longtext", nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderEntities", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_OrderEntities_CustomerEntities_CustomerUUID",
+                        column: x => x.CustomerUUID,
+                        principalTable: "CustomerEntities",
+                        principalColumn: "UUID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderEntities_ProductEntities_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "ProductEntities",
+                        principalColumn: "ID");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "KitchenQueueEntities",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    OrderID = table.Column<int>(type: "int", nullable: false),
+                    Timestamp = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_KitchenQueueEntities", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_KitchenQueueEntities_OrderEntities_OrderID",
+                        column: x => x.OrderID,
+                        principalTable: "OrderEntities",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomerEntities_TableID",
+                table: "CustomerEntities",
+                column: "TableID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_KitchenQueueEntities_OrderID",
+                table: "KitchenQueueEntities",
+                column: "OrderID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderEntities_CustomerUUID",
+                table: "OrderEntities",
+                column: "CustomerUUID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderEntities_ProductID",
+                table: "OrderEntities",
+                column: "ProductID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConfigurationEntities");
+
+            migrationBuilder.DropTable(
+                name: "KitchenQueueEntities");
+
+            migrationBuilder.DropTable(
+                name: "OrderEntities");
+
+            migrationBuilder.DropTable(
+                name: "CustomerEntities");
+
+            migrationBuilder.DropTable(
+                name: "ProductEntities");
+
+            migrationBuilder.DropTable(
+                name: "TableEntities");
+        }
+    }
+}
