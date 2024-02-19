086b98f8a394da78c62be224528a79d9e8aa2a56
Antonin <flowythe@gmail.com>
2024-02-19 19:19:28 +0100

    zodios

--- frontend/src/modules/api/zodios/zodios-client.ts
+++ frontend/src/modules/api/zodios/zodios-client.ts
@@ -4,21 +4,17 @@ import { env } from "~/env";
 
 import { z } from "zod";
 
-export const KitchenQueueEntity = z
+export const TableEntity = z
   .object({
-    uuid: z.string().nullable(),
-    productID: z.string().nullable(),
-    note: z.string().nullable(),
-    timestamp: z.number().int().nullable(),
+    id: z.number().int().nullable(),
+    name: z.string().nullable(),
+    room: z.string().nullable(),
   })
   .partial();
-export const uuid = z.string();
 export const CustomerEntity = z
   .object({
     uuid: z.string().nullable(),
-    ordered: z.array(z.string()).nullable(),
-    internalOrdered: z.string().nullable(),
-    tableID: z.string().nullable(),
+    table: TableEntity,
     creationTimestamp: z.number().int().nullable(),
   })
   .partial();
@@ -30,27 +26,135 @@ export const ProductEntity = z
     description: z.string().nullable(),
     imageURL: z.string().nullable(),
     price: z.number().int(),
+    sendToKitchenQueue: z.boolean(),
   })
   .partial();
-export const TableEntity = z
+export const OrderEntity = z
   .object({
-    id: z.number().int().nullable(),
+    id: z.number().int(),
+    customer: CustomerEntity,
+    product: ProductEntity,
+    quantity: z.number().int(),
+    timestamp: z.number().int(),
+    notes: z.string().nullable(),
+  })
+  .partial();
+export const KitchenQueueEntity = z
+  .object({
+    id: z.number().int(),
+    order: OrderEntity,
+    timestamp: z.number().int(),
+  })
+  .partial();
+export const id = z.number().int();
+export const id__2 = z.string();
+export const ProductModel = z
+  .object({
+    productID: z.string().nullable(),
     name: z.string().nullable(),
-    roomID: z.string().nullable(),
-    positionX: z.number().int(),
-    positionY: z.number().int(),
+    category: z.string().nullable(),
+    description: z.string().nullable(),
+    imageURL: z.string().nullable(),
+    price: z.number().int(),
+    sendToKitchenQueue: z.boolean(),
+  })
+  .partial();
+export const TableModel = z.object({ room: z.string().nullable() }).partial();
+export const CustomerModel = z.object({ tableID: z.number().int() }).partial();
+export const OrderModel = z
+  .object({
+    customerUUID: z.string().nullable(),
+    productID: z.string().nullable(),
+    quantity: z.number().int(),
+    notes: z.string().nullable(),
   })
   .partial();
 
 export const schemas = {
-  KitchenQueueEntity,
-  uuid,
+  TableEntity,
   CustomerEntity,
   ProductEntity,
-  TableEntity,
+  OrderEntity,
+  KitchenQueueEntity,
+  id,
+  id__2,
+  ProductModel,
+  TableModel,
+  CustomerModel,
+  OrderModel,
 };
 
 const endpoints = makeApi([
+  {
+    method: "post",
+    path: "/customer",
+    alias: "postCustomer",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: CustomerModel,
+      },
+    ],
+    response: z.void(),
+  },
+  {
+    method: "get",
+    path: "/customer/:uuid",
+    alias: "getCustomerUuid",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "uuid",
+        type: "Path",
+        schema: id__2,
+      },
+    ],
+    response: CustomerEntity,
+  },
+  {
+    method: "delete",
+    path: "/customer/:uuid",
+    alias: "deleteCustomerUuid",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "uuid",
+        type: "Path",
+        schema: id__2,
+      },
+    ],
+    response: z.void(),
+  },
+  {
+    method: "get",
+    path: "/customer/:uuid/orders",
+    alias: "getCustomerUuidorders",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "uuid",
+        type: "Path",
+        schema: id__2,
+      },
+    ],
+    response: z.array(OrderEntity),
+  },
+  {
+    method: "get",
+    path: "/customer/:uuid/orders/total",
+    alias: "getCustomerUuidorderstotal",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "uuid",
+        type: "Path",
+        schema: id__2,
+      },
+    ],
+    response: z.number().int(),
+  },
   {
     method: "get",
     path: "/customers",
@@ -59,45 +163,10 @@ const endpoints = makeApi([
     response: z.array(CustomerEntity),
   },
   {
-    method: "post",
-    path: "/customers",
-    alias: "postCustomers",
-    requestFormat: "json",
-    parameters: [
-      {
-        name: "body",
-        type: "Body",
-        schema: CustomerEntity,
-      },
-    ],
-    response: z.void(),
-  },
-  {
-    method: "get",
-    path: "/customers/:uuid",
-    alias: "getCustomersUuid",
-    requestFormat: "json",
-    parameters: [
-      {
-        name: "uuid",
-        type: "Path",
-        schema: uuid,
-      },
-    ],
-    response: CustomerEntity,
-  },
-  {
     method: "delete",
-    path: "/customers/:uuid",
-    alias: "deleteCustomersUuid",
+    path: "/customers/clear",
+    alias: "deleteCustomersclear",
     requestFormat: "json",
-    parameters: [
-      {
-        name: "uuid",
-        type: "Path",
-        schema: uuid,
-      },
-    ],
     response: z.void(),
   },
   {
@@ -108,66 +177,129 @@ const endpoints = makeApi([
     response: z.array(KitchenQueueEntity),
   },
   {
-    method: "post",
-    path: "/kitchenQueue",
-    alias: "postKitchenQueue",
+    method: "delete",
+    path: "/kitchenQueue/clear",
+    alias: "deleteKitchenQueueclear",
     requestFormat: "json",
-    parameters: [
-      {
-        name: "body",
-        type: "Body",
-        schema: KitchenQueueEntity,
-      },
-    ],
     response: z.void(),
   },
   {
     method: "get",
-    path: "/kitchenQueue/:uuid",
-    alias: "getKitchenQueueUuid",
+    path: "/kitchenQueueItem/:id",
+    alias: "getKitchenQueueItemId",
     requestFormat: "json",
     parameters: [
       {
-        name: "uuid",
+        name: "id",
         type: "Path",
-        schema: uuid,
+        schema: id,
       },
     ],
     response: KitchenQueueEntity,
   },
   {
     method: "delete",
-    path: "/kitchenQueue/:uuid",
-    alias: "deleteKitchenQueueUuid",
+    path: "/kitchenQueueItem/:id",
+    alias: "deleteKitchenQueueItemId",
     requestFormat: "json",
     parameters: [
       {
-        name: "uuid",
+        name: "id",
         type: "Path",
-        schema: uuid,
+        schema: id__2,
       },
     ],
     response: z.void(),
   },
   {
     method: "get",
-    path: "/kitchenQueue/:uuid/customer",
-    alias: "getKitchenQueueUuidcustomer",
+    path: "/kitchenQueueItem/:id/customer",
+    alias: "getKitchenQueueItemIdcustomer",
     requestFormat: "json",
     parameters: [
       {
-        name: "uuid",
+        name: "id",
         type: "Path",
-        schema: uuid,
+        schema: id,
       },
     ],
     response: CustomerEntity,
   },
   {
-    method: "delete",
-    path: "/kitchenQueue/clear",
-    alias: "deleteKitchenQueueclear",
+    method: "post",
+    path: "/order",
+    alias: "postOrder",
     requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: OrderModel,
+      },
+    ],
+    response: z.void(),
+  },
+  {
+    method: "get",
+    path: "/order/:id",
+    alias: "getOrderId",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "id",
+        type: "Path",
+        schema: id,
+      },
+    ],
+    response: OrderEntity,
+  },
+  {
+    method: "get",
+    path: "/order/:id/customer",
+    alias: "getOrderIdcustomer",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "id",
+        type: "Path",
+        schema: id,
+      },
+    ],
+    response: CustomerEntity,
+  },
+  {
+    method: "get",
+    path: "/orders",
+    alias: "getOrders",
+    requestFormat: "json",
+    response: z.array(OrderEntity),
+  },
+  {
+    method: "post",
+    path: "/product",
+    alias: "postProduct",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: ProductModel,
+      },
+    ],
+    response: z.void(),
+  },
+  {
+    method: "delete",
+    path: "/product/:id",
+    alias: "deleteProductId",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "id",
+        type: "Path",
+        schema: id__2,
+      },
+    ],
     response: z.void(),
   },
   {
@@ -179,7 +311,7 @@ const endpoints = makeApi([
       {
         name: "uuid",
         type: "Path",
-        schema: uuid,
+        schema: id__2,
       },
     ],
     response: ProductEntity,
@@ -192,38 +324,31 @@ const endpoints = makeApi([
     response: z.array(ProductEntity),
   },
   {
-    method: "post",
-    path: "/products",
-    alias: "postProducts",
+    method: "get",
+    path: "/products/categories",
+    alias: "getProductscategories",
     requestFormat: "json",
-    parameters: [
-      {
-        name: "body",
-        type: "Body",
-        schema: ProductEntity,
-      },
-    ],
-    response: z.void(),
-  },
-  {
-    method: "delete",
-    path: "/products/:id",
-    alias: "deleteProductsId",
-    requestFormat: "json",
-    parameters: [
-      {
-        name: "id",
-        type: "Path",
-        schema: uuid,
-      },
-    ],
-    response: z.void(),
+    response: z.array(z.string()),
   },
   {
     method: "delete",
     path: "/products/clear",
     alias: "deleteProductsclear",
     requestFormat: "json",
+    response: z.void(),
+  },
+  {
+    method: "post",
+    path: "/table",
+    alias: "postTable",
+    requestFormat: "json",
+    parameters: [
+      {
+        name: "body",
+        type: "Body",
+        schema: TableModel,
+      },
+    ],
     response: z.void(),
   },
   {
@@ -235,7 +360,7 @@ const endpoints = makeApi([
       {
         name: "id",
         type: "Path",
-        schema: uuid,
+        schema: id__2,
       },
     ],
     response: TableEntity,
@@ -249,7 +374,7 @@ const endpoints = makeApi([
       {
         name: "id",
         type: "Path",
-        schema: uuid,
+        schema: id,
       },
     ],
     response: z.array(CustomerEntity),
@@ -262,20 +387,6 @@ const endpoints = makeApi([
     response: z.array(TableEntity),
   },
   {
-    method: "post",
-    path: "/tables",
-    alias: "postTables",
-    requestFormat: "json",
-    parameters: [
-      {
-        name: "body",
-        type: "Body",
-        schema: TableEntity,
-      },
-    ],
-    response: z.void(),
-  },
-  {
     method: "get",
     path: "/tables/:room",
     alias: "getTablesRoom",
@@ -284,10 +395,17 @@ const endpoints = makeApi([
       {
         name: "room",
         type: "Path",
-        schema: uuid,
+        schema: id__2,
       },
     ],
     response: z.array(TableEntity),
+  },
+  {
+    method: "get",
+    path: "/tables/rooms",
+    alias: "getTablesrooms",
+    requestFormat: "json",
+    response: z.array(z.string()),
   },
 ]);
 
