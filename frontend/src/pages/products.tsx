666797e6f515724cc69da64ef38e0b57fbfe5398
Antonín <flowythe@gmail.com>
2024-02-19 19:25:03 +0100

    moved navigation to the right

--- frontend/src/pages/products.tsx
+++ frontend/src/pages/products.tsx
@@ -5,10 +5,10 @@ import { ProductsList } from '~products';
 export default function Products() {
 	return (
 		<Flex>
-			<Navigation />
-			<Flex direction={'column'} p={'1rem'}>
+			<Flex direction={'column'} w={'100%'} p={'1rem'}>
 				<ProductsList />
 			</Flex>
+			<Navigation />
 		</Flex>
 	);
 }
