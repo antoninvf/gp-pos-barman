4e768e7e478c502fde662ac9345f3557b76aefaf
Antonín <71491435+antoninvf@users.noreply.github.com>
2024-02-22 09:04:22 +0100

    index files

--- frontend/src/modules/customers/hooks/index.ts
+++ frontend/src/modules/customers/hooks/index.ts
@@ -1,1 +1,2 @@
 export * from './useCreateCustomer';
+export * from './useClearCustomers';
--- frontend/src/modules/kitchenQueue/hooks/index.ts
+++ frontend/src/modules/kitchenQueue/hooks/index.ts
@@ -1,3 +1,2 @@
-export * from '../../orders/hooks/useCreateOrder';
 export * from './useClearKitchenQueue';
 export * from './useRemoveFromKitchenQueue';
