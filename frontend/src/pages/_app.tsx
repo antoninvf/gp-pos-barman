4f45bf8b9510e46dd9c04b38148bdbba567aafad
Antonín <flowythe@gmail.com>
2024-03-02 21:20:23 +0100

    added authentication and custom colors

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,22 +1,79 @@
 import '@mantine/core/styles.css';
 import '@mantine/notifications/styles.css';
-import { MantineProvider } from '@mantine/core';
+import {
+	MantineProvider,
+	createTheme,
+	type MantineColorsTuple,
+} from '@mantine/core';
 import { Notifications } from '@mantine/notifications';
 import { ModalsProvider } from '@mantine/modals';
-import { theme } from '../../theme';
-import { QueryClientProvider } from '../modules/api';
+import { QueryClientProvider } from '~api';
+import { SessionProvider, useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+import '@fontsource/lexend';
 
-export default function App({ Component, pageProps }: any) {
+export default function App({
+	Component,
+	pageProps: { session, ...pageProps },
+}: any) {
+	// set page title for all pages
+	useEffect(() => {
+		document.title = 'Barman';
+	}, []);
+
+	const myColor: MantineColorsTuple = [
+		'#f2f0ff',
+		'#e0dff2',
+		'#bfbdde',
+		'#9b98ca',
+		'#7d79ba',
+		'#6a65b0',
+		'#605bac',
+		'#504c97',
+		'#464388',
+		'#3b3979',
+	];
+
+	const theme = createTheme({
+		fontFamily: 'Lexend',
+		colors: {
+			myColor,
+		},
+		primaryColor: 'myColor',
+	});
+
 	return (
 		<QueryClientProvider>
 			<MantineProvider theme={theme}>
 				<ModalsProvider>
 					<Notifications />
-					<main>
-						<Component {...pageProps} />
-					</main>
+					<SessionProvider
+						session={session}
+						refetchInterval={30}
+						refetchOnWindowFocus={false}
+					>
+						{Component.auth ? (
+							<Auth>
+								<Component {...pageProps} />
+							</Auth>
+						) : (
+							<Component {...pageProps} />
+						)}
+					</SessionProvider>
 				</ModalsProvider>
 			</MantineProvider>
 		</QueryClientProvider>
 	);
+}
+
+function Auth({ children }: { children: React.ReactNode }) {
+	// if `{ required: true }` is supplied, `status` can only be "loading" or "authenticated"
+	const { status } = useSession({ required: true });
+
+	if (status === 'loading') {
+		console.log(status);
+		return null;
+	}
+
+	return children;
 }
