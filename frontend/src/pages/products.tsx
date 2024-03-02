0a1207fa457da91d9f6b6c79d8f713ad35da8e21
Antonín <flowythe@gmail.com>
2024-03-02 21:15:00 +0100

    products page update

--- frontend/src/pages/products.tsx
+++ frontend/src/pages/products.tsx
@@ -1,14 +1,19 @@
 import { Flex } from '@mantine/core';
+import { ProtectedPage } from '~api';
 import Navigation from '~components/SidebarNav/Navigation';
 import { ProductsList } from '~products';
+import { CreateProductButton } from '../modules/products/components/CreateProductButton/CreateProductButton';
 
 export default function Products() {
 	return (
-		<Flex>
-			<Navigation />
-			<Flex direction={'column'} w={'100%'} p={'1rem'}>
-				<ProductsList />
+		<ProtectedPage>
+			<Flex>
+				<Navigation />
+				<CreateProductButton />
+				<Flex direction={'column'} w={'80%'} p={'1rem'}>
+					<ProductsList />
+				</Flex>
 			</Flex>
-		</Flex>
+		</ProtectedPage>
 	);
 }
