e877268795964a6d504b8a0355b1007af74fd21e
Antonín <flowythe@gmail.com>
2024-03-04 23:59:38 +0100

    made product item be one table row

--- frontend/src/modules/products/components/ProductItem/ProductItem.tsx
+++ frontend/src/modules/products/components/ProductItem/ProductItem.tsx
@@ -1,35 +1,79 @@
-import { Badge, Card, Flex, Text, Image, Button } from '@mantine/core';
-import Link from 'next/link';
+import { Flex, Table, ActionIcon, Modal, Button, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconEdit, IconTrash } from '@tabler/icons-react';
+import { useState } from 'react';
+import { useDeleteProduct } from '~products/hooks';
 
 interface IProductItemProps {
 	id?: string | null;
 	name?: string | null;
 	category?: string | null;
-	description?: string | null;
-	imageURL?: string | null;
 	price?: number | null;
+	currencyData?: string | null;
 }
 
 export const ProductItem = ({ ...props }: IProductItemProps) => {
+	const [openedDelete, { open: openDelete, close: closeDelete }] =
+		useDisclosure(false);
+	const [checked, setChecked] = useState(false);
+
+	const { submit: submitDelete } = useDeleteProduct({
+		afterSubmit: () => {
+			closeDelete();
+		},
+		id: props.id || '',
+	});
+	const handleDeleteOpen = () => {
+		openDelete();
+	};
+
 	return (
-		<Card
-			shadow="sm"
-			padding="lg"
-			radius="sm"
-			withBorder
-			onClick={() => {
-				alert('clicked on ' + props.name + ' (' + props.id + ')');
-			}}
-		>
-			<Flex justify="space-between" align={'start'} gap={'6rem'}>
-				<Text fw={600} size="xl">
-					{props.name ? props.name : props.id}
-				</Text>
-				{<Badge color="green">{props.price}</Badge>}
-			</Flex>
-			<Text size="lg">{props.category}</Text>
-			<Image src={props.imageURL} alt="" />
-			<Text size="lg">{props.description}</Text>
-		</Card>
+		<>
+			<Table.Tr>
+				<Table.Td>{props.id}</Table.Td>
+				<Table.Td>{props.name}</Table.Td>
+				<Table.Td>{props.category}</Table.Td>
+				<Table.Td>
+					{props.price} {props.currencyData}
+				</Table.Td>
+				<Table.Td>
+					<Flex gap={'sm'} align={'center'}>
+						<ActionIcon
+							size={'2.5rem'}
+							onClick={() => alert('fuck ' + props.name)}
+						>
+							<IconEdit size={'1.5rem'} />
+						</ActionIcon>
+						<ActionIcon
+							color="red"
+							size={'2.5rem'}
+							onClick={() => handleDeleteOpen()}
+						>
+							<IconTrash size={'1.5rem'} />
+						</ActionIcon>
+					</Flex>
+				</Table.Td>
+			</Table.Tr>
+
+			{/* Delete modal */}
+			<Modal
+				title="Delete product"
+				opened={openedDelete}
+				onClose={closeDelete}
+				padding={'xl'}
+				centered
+			>
+				<Text>Are you sure you want to delete the product {props?.name}?</Text>
+				<Flex justify={'right'} mt={'1rem'} gap={'xs'}>
+					<Button
+						variant={'filled'}
+						color={'red'}
+						onClick={() => submitDelete()}
+					>
+						Delete product
+					</Button>
+				</Flex>
+			</Modal>
+		</>
 	);
 };
