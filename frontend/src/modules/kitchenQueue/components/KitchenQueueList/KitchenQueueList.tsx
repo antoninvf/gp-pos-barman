6615d94524fddc81cbc2d39efef6219a12f5024b
Antonín <flowythe@gmail.com>
2024-03-06 12:20:32 +0100

    worst bug ever i dont even know

--- frontend/src/modules/kitchenQueue/components/KitchenQueueList/KitchenQueueList.tsx
+++ frontend/src/modules/kitchenQueue/components/KitchenQueueList/KitchenQueueList.tsx
@@ -1,7 +1,7 @@
 import { Flex, Title } from '@mantine/core';
 import { IconClockPause } from '@tabler/icons-react';
 import { apiHooks } from '~/api';
-import { KitchenQueueItem } from '../KitchenQueueItem';
+import { KitchenQueueItem } from '~kitchenQueue';
 
 interface IKitchenQueueProps {}
 
---
+++ frontend/src/modules/kitchenQueue/components/index.ts
@@ -0,0 +1,2 @@
+export * from './KitchenQueueItem';
+export * from './KitchenQueueList';
