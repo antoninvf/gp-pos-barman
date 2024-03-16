7675add59d4288bb55187f5d8e2fc8f1624407b3
Antonín <flowythe@gmail.com>
2024-03-16 20:29:38 +0100

    customercard fixed and overhauled

--- frontend/src/modules/customers/components/CustomerCard/CustomerCard.tsx
+++ frontend/src/modules/customers/components/CustomerCard/CustomerCard.tsx
@@ -1,46 +1,41 @@
 import { Flex, Text, Paper, ScrollArea } from '@mantine/core';
 import { apiHooks } from '~/api';
+import { OrderItem } from '~orders';
 
 interface ICustomerCardProps {
 	customerUUID?: string | null;
+	currencyData?: string | null;
 }
 
 export const CustomerCard = ({ ...props }: ICustomerCardProps) => {
 	if (!props.customerUUID) return null;
 
-	const { data: currencyData } = apiHooks.useGetConfigurationSettingName({
-		params: { settingName: 'currency' },
-	});
-
 	const { data: orderData } = apiHooks.useGetCustomerUuidorders({
-		params: { uuid: props.customerUUID },
-	});
-
-	const { data: totalOrderPrice } = apiHooks.useGetCustomerUuidorderstotal({
 		params: { uuid: props.customerUUID },
 	});
 
 	return (
 		<>
-			<Paper w={'100%'} h={'100%'} p={'md'} withBorder radius={0} shadow="0">
-				<Flex direction={'column'} justify={'space-between'} h={'100%'}>
-					<ScrollArea w={'100%'} h={'50vh'} type="auto">
-						{orderData?.map((x) => (
-							<Flex key={x.id} justify={'space-between'} align={'center'}>
-								<Text>{x.product?.name}</Text>
-								<Text c="black">
-									{x.product?.price} {currencyData}
-								</Text>
-							</Flex>
-						))}
-					</ScrollArea>
-					<Flex justify={'space-between'} align={'center'}>
-						<Text fw={'600'}>Total:</Text>
-						<Text fw={'600'}>
-							{totalOrderPrice} {currencyData}
-						</Text>
-					</Flex>
-				</Flex>
+			<Paper
+				w={'100%'}
+				h={'100%'}
+				p={'md'}
+				pr={0}
+				withBorder
+				radius={0}
+				shadow="0"
+			>
+				<ScrollArea w={'100%'} h={'100%'} type="auto" mah={'60vh'}>
+					{orderData?.map((x) => (
+						<OrderItem
+							key={x.id}
+							id={x.id || 0}
+							productName={x.product?.name || ''}
+							productPrice={x.product?.price || 0}
+							currencyData={props.currencyData}
+						/>
+					))}
+				</ScrollArea>
 			</Paper>
 		</>
 	);
