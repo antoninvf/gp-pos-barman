8d28e2d025cebd59912826d37e5775ce83ae22f4
Antonín <flowythe@gmail.com>
2024-03-16 20:27:21 +0100

    App settings done

--- frontend/src/pages/appSettings.tsx
+++ frontend/src/pages/appSettings.tsx
@@ -1,29 +1,15 @@
-import { Button, Flex } from '@mantine/core';
+import { Flex } from '@mantine/core';
 import Navigation from '~components/Navigation/Navigation';
 import { ProtectedPage } from '~api';
-import { useClearCustomers } from '~customers';
-import { useClearKitchenQueue } from '~kitchenQueue';
+import { ConfigurationList } from '~configuration';
 
 export default function AppSettings() {
-	const { submit: clearKitchenQueue } = useClearKitchenQueue({
-		afterSubmit: () => {},
-	});
-
-	const { submit: clearAllCustomers } = useClearCustomers({
-		afterSubmit: () => {},
-	});
-
 	return (
 		<ProtectedPage>
 			<Flex>
 				<Navigation />
 				<Flex direction={'column'} gap={'sm'} p={'1rem'}>
-					<Button onClick={() => clearKitchenQueue()} color="red">
-						Clear kitchen queue
-					</Button>
-					<Button onClick={() => clearAllCustomers()} color="red">
-						Clear all customers
-					</Button>
+					<ConfigurationList />
 				</Flex>
 			</Flex>
 		</ProtectedPage>
